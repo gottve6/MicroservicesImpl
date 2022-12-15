cfc376769f34e70d867e2a8dfaad997ce3589ce4
gottve6 <73166308+gottve6@users.noreply.github.com>
2022-12-15 17:59:30 +0530

    AWS S3 project added

---
+++ s3-file-storage-example/src/main/java/com/dxc/controller/StorageController.java
@@ -0,0 +1,44 @@
+package com.dxc.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dxc.service.StorageService;
+
+@RestController
+@RequestMapping("/file")
+public class StorageController {
+
+    @Autowired
+    private StorageService service;
+
+    // http://localhost:9999/file/upload
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    // http://localhost:9999/file/download/1671098929376_java_image
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    // // http://localhost:9999/file/delete/1671098929376_java_image
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
